5334c9cfe732a5fa661372809cd6a1dad72c5c04
DenisReaver <145904927+DenisReaver@users.noreply.github.com>
2025-12-27 15:15:01 +0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,19 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
-  reactCompiler: true,
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // Отключаем строгую CSP, чтобы wagmi работал
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value: "script-src 'self' 'unsafe-eval' 'unsafe-inline'; object-src 'none';",
+          },
+        ],
+      },
+    ];
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
